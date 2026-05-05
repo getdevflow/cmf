40fd015f086e6fb3ae91a82574ca8f8d59931044
Joshua Parker <joshua@joshuaparker.dev>
2026-05-05 11:17:53 -0600

    Removed the call to non-existent seeder.
    Signed-off-by: Joshua Parker <joshua@joshuaparker.dev>

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -4,19 +4,13 @@ declare(strict_types=1);
 
 namespace Database\Seeders;
 
-use Psr\Container\ContainerExceptionInterface;
-use Psr\Container\NotFoundExceptionInterface;
 use Qubus\Expressive\Migration\Seeder\BaseSeeder;
 use Qubus\Expressive\Migration\Seeder\SeederContext;
 
 final class DatabaseSeeder extends BaseSeeder
 {
-    /**
-     * @throws ContainerExceptionInterface
-     * @throws NotFoundExceptionInterface
-     */
     public function run(SeederContext $context): void
     {
-        $this->call(seeder: UserSeeder::class, context: $context);
+        //
     }
 }
