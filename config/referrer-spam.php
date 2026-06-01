59c640447565a7d5406d70345060948b9f465db6
Joshua Parker <joshua@joshuaparker.dev>
2026-05-31 20:00:59 -0600

    Updated commented block and set to null to use Matomo referrer spam blacklist.
    Signed-off-by: Joshua Parker <joshua@joshuaparker.dev>

--- config/referrer-spam.php
+++ config/referrer-spam.php
@@ -3,7 +3,12 @@
 declare(strict_types=1);
 
 return [
-    'blacklist' => [
-        //enter a comma-delimited list
-    ]
+    /*
+    |--------------------------------------------------------------------------
+    | When set to `null` (default), it uses the Matomo referrer spam
+    | blacklist. Alternatively, you can remove `null`, and replace it
+    | with your defined array of referrer spammers to block.
+    |--------------------------------------------------------------------------
+    */
+    'blacklist' => null,
 ];
