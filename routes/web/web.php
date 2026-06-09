b739566752bcb22910b853b2dc7696a5c7fdd783
Joshua Parker <joshua@joshuaparker.dev>
2026-06-09 14:32:12 -0600

    Code beautifier.
    Signed-off-by: Joshua Parker <joshua@joshuaparker.dev>

--- routes/web/web.php
+++ routes/web/web.php
@@ -2,10 +2,6 @@
 
 declare(strict_types=1);
 
-use Psr\Http\Message\RequestInterface;
-use Qubus\Http\Request;
-
-use function Codefy\Framework\Helpers\app;
 use function Codefy\Framework\Helpers\config;
 use function Qubus\Security\Helpers\__observer;
 
@@ -15,7 +11,7 @@ return function (\Qubus\Routing\Psr7Router $router) {
     // Custom theme routes
     __observer()->filter->applyFilter('theme.route', $router);
 
-    if(config()->boolean(key: 'vihzhuo.enable')) {
+    if (config()->boolean(key: 'vihzhuo.enable')) {
         $router
             ->any(
                 uri: config()->string(key: 'vihzhuo.general.assets_url') . '{any}',
